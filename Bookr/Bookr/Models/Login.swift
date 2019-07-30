c89e7c50ab9c8f25694bab81bf454201a80c3b83
Jeffrey Santana <SantanaJny@gmail.com>
2019-07-29 20:53:20 -0400

    Connecting login endpoint to network and model

---
+++ Bookr/Bookr/Models/Login.swift
@@ -0,0 +1,14 @@
+//
+//  Login.swift
+//  Bookr
+//
+//  Created by Jeffrey Santana on 7/29/19.
+//  Copyright © 2019 Lambda. All rights reserved.
+//
+
+import Foundation
+
+struct Login: Codable {
+	let user: User
+	let token: String
+}
