9f9c4c842f7743deee66d3fa890fc15dcc7a89fe
Taylor Lyles <hawks93187@gmail.com>
2019-07-29 19:45:26 -0700

    Added collection view and TVC

---
+++ Bookr/Book.swift
@@ -0,0 +1,20 @@
+//
+//  Book.swift
+//  Bookr
+//
+//  Created by Taylor Lyles on 7/29/19.
+//  Copyright © 2019 Lambda. All rights reserved.
+//
+
+import UIKit
+
+struct Book {
+	
+	var title: String
+	var description: String
+	var author: String
+	var company: String
+	var image: UIImage
+	var rating: Double
+	
+}
---
+++ Bookr/HomePageViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HomePageViewController.swift
+//  Bookr
+//
+//  Created by Taylor Lyles on 7/29/19.
+//  Copyright © 2019 Lambda. All rights reserved.
+//
+
+import UIKit
+
+class HomePageViewController: UIViewController {
+	
+	// let bookCoontroller = BookController()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+	
+    
+
+	
+
+}
