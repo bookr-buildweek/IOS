588082fbf80fcde9492202a60f117a2e7586e362
Taylor Fahey-Lyles <hawks93187@gmail.com>
2019-07-30 18:05:53 -0700

    Added FilterCollection Cell,CVDelegate & CVData

---
+++ Bookr/Bookr/Views/FilterCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  FilterCollectionViewCell.swift
+//  Bookr
+//
+//  Created by Taylor Lyles on 7/30/19.
+//  Copyright © 2019 Lambda. All rights reserved.
+//
+
+import UIKit
+
+class FilterCollectionViewCell: UICollectionViewCell {
+	
+	@IBOutlet weak var filterImageView: UIImageView!
+	
+}
