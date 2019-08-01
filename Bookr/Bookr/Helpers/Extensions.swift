cb8e2ada147d824586ba63d94790320520f69f1b
Jeffrey Santana <SantanaJny@gmail.com>
2019-08-01 01:07:17 -0400

    Adding common alerts to viewController extension

--- Bookr/Bookr/Helpers/Extensions.swift
+++ Bookr/Bookr/Helpers/Extensions.swift
@@ -14,3 +14,22 @@ extension UITextField {
 		return (trimmedText ?? "").isEmpty ? nil : trimmedText
 	}
 }
+
+extension UIViewController {
+	func fillIn(text field: UITextField, alertMessage: String) -> String? {
+		guard let text = field.optionalText else {
+			presentInfoAlert(title: "Missing Content", message: alertMessage) { (_) in
+				field.becomeFirstResponder()
+			}
+			return nil
+		}
+		return text
+	}
+	
+	func presentInfoAlert(title: String, message: String, handler: ((UIAlertAction) -> Void)? = nil) {
+		let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+		
+		alertVC.addAction(UIAlertAction(title: "OK", style: .default, handler: handler))
+		present(alertVC, animated: true, completion: nil)
+	}
+}
